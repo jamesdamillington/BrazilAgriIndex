c5e97b33392f693c5be8941ecb9f5af09681b835
jamesdamillington@gmail.com <jamesdamillington@gmail.com>
2019-05-21 20:22:40 +0100

    added functionality to examine spcific 'seasons' (i.e. months within a year)

--- agriIndex.r
+++ agriIndex.r
@@ -245,7 +245,7 @@ fn_fromYearVar <- function(year, var)
 BRA.ext <- extent(-62.39713, -35.43949, -33.89756, -4.06125)
 
 
-calcAgriMaps <- function(munis.r, PAW, year, BRA.e, hemi)
+calcAgriMaps <- function(munis.r, PAW, year, BRA.e, hemi, season)
 {
   #generate timeRange and filenames for this year
   tr <- y_fromYear(year)
@@ -256,6 +256,11 @@ calcAgriMaps <- function(munis.r, PAW, year, BRA.e, hemi)
   #month labels for plots
   monlab <- c(paste0("Jan",year),paste0("Feb",year),paste0("Mar",year),paste0("Apr",year),paste0("May",year),paste0("Jun",year),paste0("Jul",year),paste0("Aug",year),paste0("Sep",year),paste0("Oct",year),paste0("Nov",year),paste0("Dec",year))
   if(hemi == "S")  monlab <- c(paste0("Jul",year-1),paste0("Aug",year-1),paste0("Sep",year-1),paste0("Oct",year-1),paste0("Nov",year-1),paste0("Dec",year-1),paste0("Jan",year),paste0("Feb",year),paste0("Mar",year),paste0("Apr",year),paste0("May",year),paste0("Jun",year))
+  
+  #create season filelabel
+  season_label <- paste0(season, collapse="")
+  if(hemi == "S" && season_label == "JulAugSepOctNovDecJanFebMarAprMayJun") season_label <- "All"
+  if(hemi == "N" && season_label == "JanFebMarAprMayJunJulAugSepOctNovDec") season_label <- "All"
   
   
   #for testing
@@ -408,78 +413,104 @@ calcAgriMaps <- function(munis.r, PAW, year, BRA.e, hemi)
   
   }
   
-  names(DEF.b) <- monlab
+  names(DEF.b) <- monlab    #apply month-year names to the layes (for plotting and access below)
   names(ET.b) <- monlab
 
-  #calculate Dryness Index
-  avDEF<-mean(DEF.b)#mean annual DEF
-  avPET<-mean(PET.b)#mean annual PET
-  avDi <- (100*avDEF) / avPET
+  si_yr <- lapply(season, paste0, year)  #add the year to month names to match monlab format
+  season_indices <- ifelse(monlab %in% si_yr,1,2)  #create index of months to use in stackApply below (1 is in season, 2 is not)
   
+  #calculate Dryness Index
+  #avDEF<-mean(DEF.b)#mean annual DEF
+  avDEF <- stackApply(DEF.b, season_indices, mean)  #mean DEF (for specified months), #creates a stack of two layers (season months and non-season months)
+  #avPET<-mean(PET.b)#mean annual PET
+  avPET <- stackApply(PET.b, season_indices, mean)  #mean PET (for specified months), #creates a stack of two layers (season months and non-season months)
+
+  avDi <- (100*avDEF) / avPET  #creates a stack of two layers (season months and non-season months)
   Di <- (100*DEF.b) / PET.b
   
   
-  #Number of months with water deficit
-  #helper function
-  countWD <- function(vect)
-  {
-    return(sum(vect > 5))
-  }
+  #pptn and temp by season if needed
+  avPptn <- stackApply(pre.b, season_indices, mean)
+  avTemp <- stackApply(avtemp.b, season_indices, mean)
   
-  DEFmonths <- calc(DEF.b, countWD)
-  Stoidiffc <- allmeanStoi[12] - allmeanStoi[1]
+  
+  #Number of months with water deficit - helper function
+  countWD <- function(vect, na.rm=T) { return(sum(vect > 5)) }
+  
+  #Number of months in the season - helper function
+  countMonths <- function(vect, na.rm=T) { return(length(vect)) }
+
+  #calculate various ways of defining water deficif months
+  DEFmonths <- stackApply(DEF.b,season_indices, countWD)  #creates a stack of two layers (season months and non-season months)
+  allmonths <- stackApply(DEF.b,season_indices, countMonths) #creates a stack of two layers (season months and non-season months)
+  DEFmonths_prop <- DEFmonths / allmonths   #proportion,  #creates a stack of two layers (season months and non-season months)
+  
+  #Stoidiffc <- allmeanStoi[12] - allmeanStoi[1]  #this does not seem to be used elsewhere...
   #Stoidiffc
   
   #write data to files
   if(writeClimRast)
   {
-    writeRaster(avDEF, paste0(outputDir,"/",className,"/MeanAnnualDEF",year,".asc"), format = 'ascii', overwrite=T)
-    writeRaster(avPET, paste0(outputDir,"/",className,"/MeanAnnualPET",year,".asc"), format = 'ascii', overwrite=T)
-    writeRaster(Ta, paste0(outputDir,"/",className,"/MeanAnnualTemp",year,".asc"), format = 'ascii', overwrite=T)
-    writeRaster(Pa, paste0(outputDir,"/",className,"/MeanAnnualPrecip",year,".asc"), format = 'ascii', overwrite=T)
-    writeRaster(avDi, paste0(outputDir,"/",className,"/MeanAnnualDI",year,".asc"), format = 'ascii', overwrite=T)
-    writeRaster(DEFmonths, paste0(outputDir,"/",className,"/CountDEFmonths",year,".asc"), format = 'ascii', overwrite=T)
+    writeRaster(avDEF[["index_1"]], paste0(outputDir,"/",className,"/MeanDEF_",season_label,"_",year,hemi,".asc"), format = 'ascii', overwrite=T)
+    writeRaster(avPET[["index_1"]], paste0(outputDir,"/",className,"/MeanPET_",season_label,"_",year,hemi,".asc"), format = 'ascii', overwrite=T)
+    writeRaster(avTemp[["index_1"]], paste0(outputDir,"/",className,"/MeanTemp_",season_label,"_",year,hemi,".asc"), format = 'ascii', overwrite=T)
+    writeRaster(avPptn[["index_1"]], paste0(outputDir,"/",className,"/MeanPrecip_",season_label,"_",year,hemi,".asc"), format = 'ascii', overwrite=T)
+    writeRaster(avDi[["index_1"]], paste0(outputDir,"/",className,"/MeanDI_",season_label,"_",year,hemi,".asc"), format = 'ascii', overwrite=T)
+    writeRaster(DEFmonths[["index_1"]], paste0(outputDir,"/",className,"/CountDEFmonths_",season_label,"_",year,hemi,".asc"), format = 'ascii', overwrite=T)
+    writeRaster(DEFmonths_prop[["index_1"]], paste0(outputDir,"/",className,"/PropDEFmonths_",season_label,"_",year,hemi,".asc"), format = 'ascii', overwrite=T)
   }
   
   #write pdfs
   if(writeClimPdf)
   {
-    pdf(paste0(outputDir,"/",className,"/mDEF",year,".pdf"))
+
+    pdf(paste0(outputDir,"/",className,"/DEF_",year,hemi,".pdf"))
     plot(DEF.b, ext = BRA.e)
     dev.off()
-    
-    pdf(paste0(outputDir,"/",className,"/mPET",year,".pdf"))
+
+    pdf(paste0(outputDir,"/",className,"/PET_",year,hemi,".pdf"))
     plot(PET.b, ext = BRA.e)
     dev.off()
 
-    pdf(paste0(outputDir,"/",className,"/mET",year,".pdf"))
+    pdf(paste0(outputDir,"/",className,"/ET_",year,hemi,".pdf"))
     plot(ET.b, ext = BRA.e)
     dev.off()
-    
-    pdf(paste0(outputDir,"/",className,"/mPPTN",year,".pdf"))
+
+    pdf(paste0(outputDir,"/",className,"/PPTN_",year,hemi,".pdf"))
     plot(pre.b, ext = BRA.e)
     dev.off()
-    
-    pdf(paste0(outputDir,"/",className,"/mmeanDEF",year,".pdf"))
-    plot(avDEF, ext = BRA.e)
-    dev.off()
-    
-    pdf(paste0(outputDir,"/",className,"/mmeanPET",year,".pdf"))
-    plot(avPET, ext = BRA.e)
-    dev.off()
-    
-    pdf(paste0(outputDir,"/",className,"/mDI",year,".pdf"))
+
+    pdf(paste0(outputDir,"/",className,"/DI_",year,hemi,".pdf"))
     plot(Di, ext = BRA.e)
     dev.off()
     
-    pdf(paste0(outputDir,"/",className,"/mmeanDI",year,".pdf"))
-    plot(avDi, ext = BRA.e)
+    pdf(paste0(outputDir,"/",className,"/ClimateVariables_",season_label,"_",year,hemi,".pdf"))
+    #pdf(paste0(outputDir,"/",className,"/meanDEF_",season_label,"_",year,hemi,".pdf"))
+    plot(avDEF[["index_1"]], ext = BRA.e, main=paste("meanDEF",season_label,year,hemi, sep=" "))  #need to use "index_1" to get to months labelled 1 in season_indices
+    #dev.off()
+
+    #pdf(paste0(outputDir,"/",className,"/meanPET_",season_label,"_",year,hemi,".pdf"))
+    plot(avPET[["index_1"]], ext = BRA.e, main=paste("meanPET",season_label,year,hemi, sep=" "))  #need to use "index_1" to get to months labelled 1 in season_indices
+    #dev.off()
+
+    #pdf(paste0(outputDir,"/",className,"/meanDI_",season_label,"_",year,hemi,".pdf"))
+    plot(avDi[["index_1"]], ext = BRA.e, main=paste("meanDI",season_label,year,hemi, sep=" "))  #need to use "index_1" to get to months labelled 1 in season_indices
+    #dev.off()
+    
+    #pdf(paste0(outputDir,"/",className,"/DEFmonths_",season_label,"_",year,hemi,".pdf"))
+    plot(DEFmonths[["index_1"]], ext = BRA.e, main=paste("count DEFmonths",season_label,year,hemi, sep=" "))
+    #dev.off()
+    
+    #pdf(paste0(outputDir,"/",className,"/DEFmonths_prop_",season_label,"_",year,hemi,".pdf"))
+    plot(DEFmonths_prop[["index_1"]], ext = BRA.e, main=paste("prop DEFmonths",season_label,year,hemi, sep=" "))
     dev.off()
+  
   }
   
   
-  
-
+  #####
+  #2019-05-21
+  #Switching focus to seasonal analysis means we can ignore ClimLim for now.  
 
   
   #Classify climate limitation (combination of deficit months and dryness index)
@@ -490,16 +521,20 @@ calcAgriMaps <- function(munis.r, PAW, year, BRA.e, hemi)
     #3 – Strong
     #4 – Very strong
   
-  ClimLim <- nullRaster
+  #ClimLim <- nullRaster
+
   
+   
   ###new classification
-  ClimLim[DEFmonths[]<=2]<-0
-  ClimLim[DEFmonths[]>2&DEFmonths[]<=4]<-1
-  ClimLim[DEFmonths[]>4&DEFmonths[]<=6]<-2
-  ClimLim[DEFmonths[]==7]<-3
-  ClimLim[DEFmonths[]>7]<-4
+  # ClimLim[DEFmonths[]<=2]<-0
+  # ClimLim[DEFmonths[]>2&DEFmonths[]<=4]<-1
+  # ClimLim[DEFmonths[]>4&DEFmonths[]<=6]<-2
+  # ClimLim[DEFmonths[]==7]<-3
+  # ClimLim[DEFmonths[]>7]<-4
   ###end new classification
+
   
+    
   ###original classification
   # ClimLim[DEFmonths[]<1&avDi[]<20]<-0
   # ClimLim[DEFmonths[]<1&avDi[]>=20&avDi[]<40]<-0
@@ -534,73 +569,74 @@ calcAgriMaps <- function(munis.r, PAW, year, BRA.e, hemi)
   
   #par(mfrow=c(1,1))
   #plot(ClimLim)
-  
-  writeRaster(ClimLim, paste0(outputDir,"/",className,"/ClimLim",year,".asc"), format = 'ascii', overwrite=T)
-  
-  pdf(paste0(outputDir,"/",className,"/ClimLim",year,".pdf"))
-  plot(ClimLim, ext = BRA.e)
-  dev.off()
-  
-  pdf(paste0(outputDir,"/",className,"/DEFmonths",year,".pdf"))
-  plot(DEFmonths, ext = BRA.e)
-  dev.off()
- 
-  #create map of double cropping
-  #discussion with Ramon by email in March 2019 suggested that DC is not possible on limitation 3 and 4
-  dc.f <- ClimLim
-  values(dc.f)[values(dc.f)==0 | values(dc.f)==2 | values(dc.f)==3 ] = 1 
-  values(dc.f)[values(dc.f)==4] = 0 
-  #writeRaster(dc.f, paste0(outputDir,"/",className,"/DC",year,".asc"), format = 'ascii', overwrite=T)
+#uncomment  
+  # writeRaster(ClimLim, paste0(outputDir,"/",className,"/ClimLim",year,".asc"), format = 'ascii', overwrite=T)
+  # 
+  # pdf(paste0(outputDir,"/",className,"/ClimLim",year,".pdf"))
+  # plot(ClimLim, ext = BRA.e)
+  # dev.off()
+  # 
 
-  pdf(paste0(outputDir,"/",className,"/DC",year,".pdf"))
-  plot(dc.f, ext = BRA.e)
-  dev.off()
-  
+  # 
+  # #create map of double cropping
+  # #discussion with Ramon by email in March 2019 suggested that DC is not possible on limitation 3 and 4
+  # dc.f <- ClimLim
+  # values(dc.f)[values(dc.f)==0 | values(dc.f)==2 | values(dc.f)==3 ] = 1
+  # values(dc.f)[values(dc.f)==4] = 0
+  # #writeRaster(dc.f, paste0(outputDir,"/",className,"/DC",year,".asc"), format = 'ascii', overwrite=T)
+  # 
+  # pdf(paste0(outputDir,"/",className,"/DC",year,".pdf"))
+  # plot(dc.f, ext = BRA.e)
+  # dev.off()
+
   
   #Considering the two variables, we join by combining the 6 classes of slope with the 5 classes of climate, resulting in 30 combinations grouped according to the table (below).
   #Each combination is reclassified with a code accordingly to the maximum value of the combination (the value of one of the two variables at least). 
   #The code (suitability classes which equate to Agricultural Capital for CRAFTY) for the combined map was divided by 4 to represent the scale values from 0 to 1.
 
-  vagri.f <- ClimLim
-  values(vagri.f)[values(ClimLim)==0&values(vslope.m)==0] = 1      #best
-  values(vagri.f)[values(ClimLim)==0&values(vslope.m)==1] = 0.75
-  values(vagri.f)[values(ClimLim)==0&values(vslope.m)==2] = 0.5
-  values(vagri.f)[values(ClimLim)==0&values(vslope.m)==3] = 0.25
-  values(vagri.f)[values(ClimLim)==0&values(vslope.m)==4] = 0.1
-  
-  values(vagri.f)[values(ClimLim)==1&values(vslope.m)==0] = 0.75
-  values(vagri.f)[values(ClimLim)==1&values(vslope.m)==1] = 0.75
-  values(vagri.f)[values(ClimLim)==1&values(vslope.m)==2] = 0.5
-  values(vagri.f)[values(ClimLim)==1&values(vslope.m)==3] = 0.25
-  values(vagri.f)[values(ClimLim)==1&values(vslope.m)==4] = 0.1
-  
-  values(vagri.f)[values(ClimLim)==2&values(vslope.m)==0] = 0.5
-  values(vagri.f)[values(ClimLim)==2&values(vslope.m)==1] = 0.5
-  values(vagri.f)[values(ClimLim)==2&values(vslope.m)==2] = 0.5
-  values(vagri.f)[values(ClimLim)==2&values(vslope.m)==3] = 0.25
-  values(vagri.f)[values(ClimLim)==2&values(vslope.m)==4] = 0.1
-  
-  values(vagri.f)[values(ClimLim)==3&values(vslope.m)==0] = 0.25
-  values(vagri.f)[values(ClimLim)==3&values(vslope.m)==1] = 0.25
-  values(vagri.f)[values(ClimLim)==3&values(vslope.m)==2] = 0.25
-  values(vagri.f)[values(ClimLim)==3&values(vslope.m)==3] = 0.25
-  values(vagri.f)[values(ClimLim)==3&values(vslope.m)==4] = 0.1
-  
-  values(vagri.f)[values(ClimLim)==4&values(vslope.m)==0] = 0.1
-  values(vagri.f)[values(ClimLim)==4&values(vslope.m)==1] = 0.1
-  values(vagri.f)[values(ClimLim)==4&values(vslope.m)==2] = 0.1
-  values(vagri.f)[values(ClimLim)==4&values(vslope.m)==3] = 0.1
-  values(vagri.f)[values(ClimLim)==4&values(vslope.m)==4] = 0.1
-  values(vagri.f)[values(vagri.f)>1]=0                              #worst
+#uncomment
+  # vagri.f <- ClimLim
+  # values(vagri.f)[values(ClimLim)==0&values(vslope.m)==0] = 1      #best
+  # values(vagri.f)[values(ClimLim)==0&values(vslope.m)==1] = 0.75
+  # values(vagri.f)[values(ClimLim)==0&values(vslope.m)==2] = 0.5
+  # values(vagri.f)[values(ClimLim)==0&values(vslope.m)==3] = 0.25
+  # values(vagri.f)[values(ClimLim)==0&values(vslope.m)==4] = 0.1
+  # 
+  # values(vagri.f)[values(ClimLim)==1&values(vslope.m)==0] = 0.75
+  # values(vagri.f)[values(ClimLim)==1&values(vslope.m)==1] = 0.75
+  # values(vagri.f)[values(ClimLim)==1&values(vslope.m)==2] = 0.5
+  # values(vagri.f)[values(ClimLim)==1&values(vslope.m)==3] = 0.25
+  # values(vagri.f)[values(ClimLim)==1&values(vslope.m)==4] = 0.1
+  # 
+  # values(vagri.f)[values(ClimLim)==2&values(vslope.m)==0] = 0.5
+  # values(vagri.f)[values(ClimLim)==2&values(vslope.m)==1] = 0.5
+  # values(vagri.f)[values(ClimLim)==2&values(vslope.m)==2] = 0.5
+  # values(vagri.f)[values(ClimLim)==2&values(vslope.m)==3] = 0.25
+  # values(vagri.f)[values(ClimLim)==2&values(vslope.m)==4] = 0.1
+  # 
+  # values(vagri.f)[values(ClimLim)==3&values(vslope.m)==0] = 0.25
+  # values(vagri.f)[values(ClimLim)==3&values(vslope.m)==1] = 0.25
+  # values(vagri.f)[values(ClimLim)==3&values(vslope.m)==2] = 0.25
+  # values(vagri.f)[values(ClimLim)==3&values(vslope.m)==3] = 0.25
+  # values(vagri.f)[values(ClimLim)==3&values(vslope.m)==4] = 0.1
+  # 
+  # values(vagri.f)[values(ClimLim)==4&values(vslope.m)==0] = 0.1
+  # values(vagri.f)[values(ClimLim)==4&values(vslope.m)==1] = 0.1
+  # values(vagri.f)[values(ClimLim)==4&values(vslope.m)==2] = 0.1
+  # values(vagri.f)[values(ClimLim)==4&values(vslope.m)==3] = 0.1
+  # values(vagri.f)[values(ClimLim)==4&values(vslope.m)==4] = 0.1
+  # values(vagri.f)[values(vagri.f)>1]=0                              #worst
 
   #!check does this need to be resampled/masked before writing?!
-  writeRaster(vagri.f, paste0(outputDir,"/",className,"/agricultureCapital",year,".asc"), format = 'ascii', overwrite=T)
+  # writeRaster(vagri.f, paste0(outputDir,"/",className,"/agricultureCapital",year,".asc"), format = 'ascii', overwrite=T)
+  # 
+  # pdf(paste0(outputDir,"/",className,"/agricultureCapital",year,".pdf"))
+  # plot(vagri.f, ext = BRA.e)
+  # dev.off()
   
-  pdf(paste0(outputDir,"/",className,"/agricultureCapital",year,".pdf"))
-  plot(vagri.f, ext = BRA.e)
-  dev.off()
-  
-  rm(pre,tmn,tmx,pre.b,tmn.b,tmx.b,PET.b,DEF.b,ET.b,ClimLim,vagri.f)
+  rm(pre,tmn,tmx,pre.b,tmn.b,tmx.b,PET.b,DEF.b,ET.b
+    #ClimLim,vagri.f
+    )
   
 }
 
@@ -609,21 +645,25 @@ calcAgriMaps <- function(munis.r, PAW, year, BRA.e, hemi)
 outputDir <- "Output"
 className <- "class-A-SH"
 
+#crop_season <- c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")
+#soy_season <- c("Oct","Nov","Dec","Jan","Feb","Mar")
+mz1_season <- c("Oct","Nov","Dec","Jan","Feb","Mar")
+mz2_season <- c("Jan","Feb","Mar","Apr","May","Jun")
+crops <- list(mz1_season, mz2_season)
+
+
 #create the output directory for this classification if it does not exist
 if(!dir.exists(paste0(outputDir,"/",className))) { dir.create(paste0(outputDir,"/",className)) }
 
-#y <-2015
-for(y in 2005:2016)
-{
-  writeClimRast <- T
-  writeClimPdf <- F
-  calcAgriMaps(munis.r, PAW, y, BRA.ext, "S")
-  print(paste0(y," done"))
+
+#yr <-2015
+for(crop in crops) {
+  for(yr in 2003:2016)
+  {
+    writeClimRast <- T
+    writeClimPdf <- T
+    calcAgriMaps(munis.r, PAW, yr, BRA.ext, "S", crop)
+    print(paste0(yr," done"))
+  }
 }
 
-
-
-#unlink("Data/sim10_BRmunis_latlon_5km_2018-04-27.asc")
-#unlink("Data/soilT_2018-05-01.asc")
-#unlink("Data/cruts", recursive = T)
-#unlink("Data/vslope_2018-04-30.asc")
